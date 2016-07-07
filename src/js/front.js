98b409c9a29f1f206d56f1688e8481a25473791e
Vlad <parshencev.vlad@gmail.com>
2016-07-07 14:29:15 +0300

    Add front js file

---
+++ src/js/front.js
@@ -0,0 +1,5 @@
+function start () {
+	cashTimer.functions.startCashTimer();
+	document.getElementById('settings').classList.remove('active');
+	document.getElementById('view').classList.add('active');
+}
